39db00cc9dd9c653ffffa80d78cca966e4c8599f
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-10-23 12:31:52 -0300

    Buscando as trrês últimas mensagens

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
-import { CreateMessageController } from '../controllers/CreateMessageController';
 import { ensureAuthenticate } from '../middleware/ensureAuthenticate';
+
+import { CreateMessageController } from '../controllers/CreateMessageController';
+import { GetLast3MessagesController } from '../controllers/GetLast3MessagesController';
 
 export const messageRoutes = Router();
 
 messageRoutes.post('/messages', ensureAuthenticate, new CreateMessageController().handle);
+messageRoutes.get('/messages/last3', new GetLast3MessagesController().handle);
